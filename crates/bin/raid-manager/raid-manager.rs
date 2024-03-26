94ac960a77c5e9d2c46219ca4990eba77b2c9ca7
Lance Wallis <walance@pvas.me>
2024-03-26 18:51:55 -0500

    Command and Raid Team skeleton.

--- crates/bin/raid-manager/raid-manager.rs
+++ crates/bin/raid-manager/raid-manager.rs
@@ -1,13 +1,123 @@
-use std::env;
+mod commands;
+mod raid_team;
+
+use serenity::async_trait;
+use serenity::builder::{CreateAttachment, CreateEmbed, CreateEmbedFooter, CreateMessage};
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::model::Timestamp;
+use serenity::prelude::*;
+use serenity::all::GatewayIntents;
+
+use std::{
+    collections::HashMap,
+    env::var,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+// Types used by all command functions.
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+// Custom user data passed to all command functions.
+pub struct Data {
+    raid_teams: Vec<raid_team::RaidTeam>,
+}
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // Custom error handler.
+    // Only handle ones we want to customize.
+    // Forward the rest to the default handler.
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx, .. } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
 
 fn main() {
     scoped_main();
 }
 
-fn scoped_main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn scoped_main() {
+    // FrameworkOptions contains all of poise's configuration option in one struct
+    // Every option can be omitted to use its default value
+    let options = poise::FrameworkOptions {
+        commands: vec![commands::help()],
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some("/".into()),
+            edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
+                Duration::from_secs(3600),
+            ))),
+            ..Default::default()
+        },
+        // The global error handler for all error cases that may occur
+        on_error: |error| Box::pin(on_error(error)),
+        // This code is run before every command
+        pre_command: |ctx| {
+            Box::pin(async move {
+                println!("Executing command {}...", ctx.command().qualified_name);
+            })
+        },
+        // This code is run after a command if it was successful (returned Ok)
+        post_command: |ctx| {
+            Box::pin(async move {
+                println!("Executed command {}!", ctx.command().qualified_name);
+            })
+        },
+        // Every command invocation must pass this check to continue execution
+        command_check: Some(|ctx| {
+            Box::pin(async move {
+                if ctx.author().id == 123456789 {
+                    return Ok(false);
+                }
+                Ok(true)
+            })
+        }),
+        // Enforce command checks even for owners (enforced by default)
+        // Set to true to bypass checks, which is useful for testing
+        skip_checks_for_owners: false,
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(async move {
+                println!(
+                    "Got an event in event handler: {:?}",
+                    event.snake_case_name()
+                );
+                Ok(())
+            })
+        },
+        ..Default::default()
+    };
 
-    let _args: Vec<String> = env::args().collect();
+    let framework = poise::Framework::builder()
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                println!("Logged in as {}", _ready.user.name);
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {
+                    raid_teams: Vec::new()
+                })
+            })
+        })
+        .options(options)
+        .build();
 
-    
+    let token = var("DISCORD_TOKEN")
+        .expect("Missing `DISCORD_TOKEN` env var, see README for more information.");
+    let intents =
+        GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+
+    let client = serenity::all::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap()
 }
---
+++ crates/bin/raid-manager/raid_team.rs
@@ -0,0 +1,5 @@
+pub struct RaidTeam {
+    team_name: String,
+    raid_lead: String,
+    raid_colead: String,
+}
