7758939575be62e6a9f0550c82adaa48103938ef
Lance Wallis <walance@pvas.me>
2024-05-28 00:13:10 -0500

    Database testing, joinability.

--- crates/lib/ogrm/guild/schema.rs
+++ crates/lib/ogrm/guild/schema.rs
@@ -7,6 +7,13 @@ diesel::table! {
 }
 
 diesel::table! {
+    raidleads (id) {
+        id -> Int8,
+        team -> Text,
+    }
+}
+
+diesel::table! {
     raidteams (id) {
         id -> Int8,
         guild_id -> Int8,
@@ -18,5 +25,6 @@ diesel::joinable!(raidteams -> guilds (guild_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
     guilds,
+    raidleads,
     raidteams,
 );
--- migrations/2024-05-28-041957_guilds/up.sql
+++ migrations/2024-05-28-041957_guilds/up.sql
@@ -5,6 +5,11 @@ CREATE TABLE guilds (
 CREATE TABLE raidteams (
     id BIGSERIAL PRIMARY KEY,
     guild_id BIGSERIAL NOT NULL REFERENCES guilds(id),
-    team_name TEXT NOT NULL
+    team_name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE raidleads (
+    id BIGSERIAL PRIMARY KEY,
+    team TEXT UNIQUE NOT NUll REFERENCES raidteams(team_name)
 );
 
